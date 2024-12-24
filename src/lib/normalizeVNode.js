518c1feed0dd8f63b25f5e57f5f78a079955a77a
taeyoung <terry.hwang@h2ohospitality.io>
2024-12-24 18:44:40 +0900

    normalizeVNode 구현중 reset 할것

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,27 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return vNode.toString();
+  }
+
+  if (typeof vNode === "boolean" || vNode === null || vNode === undefined) {
+    return "";
+  }
+
+  const { type, props = {}, children = [] } = vNode;
+  if (typeof type === "function") {
+    return normalizeVNode(
+      type({ ...props, children: children.map(normalizeVNode) }),
+    );
+  }
+  console.log(children.filter((child) => !child));
+  return {
+    type,
+    props,
+    children: children
+      .filter(
+        (child) =>
+          (typeof child === "boolean") | (child === 0) || Boolean(child),
+      )
+      .map(normalizeVNode),
+  };
 }
